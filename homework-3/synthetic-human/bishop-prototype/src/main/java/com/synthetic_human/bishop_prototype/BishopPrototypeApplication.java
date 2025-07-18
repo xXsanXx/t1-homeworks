08e3c9f858b23d0016d63c740730311208d1e368
Nastena <anastasia.stepanovich@outlook.com>
2025-07-18 19:18:10 +0300

    Create project

---
+++ homework-3/synthetic-human/bishop-prototype/src/main/java/com/synthetic_human/bishop_prototype/BishopPrototypeApplication.java
@@ -0,0 +1,13 @@
+package com.synthetic_human.bishop_prototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BishopPrototypeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BishopPrototypeApplication.class, args);
+    }
+
+}
---
+++ homework-3/synthetic-human/bishop-prototype/src/test/java/com/synthetic_human/bishop_prototype/BishopPrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synthetic_human.bishop_prototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BishopPrototypeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
